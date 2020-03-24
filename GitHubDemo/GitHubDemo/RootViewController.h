61bb4609877951becddc91cf5c23e9ca653439e7
ryan.huang <>
2020-03-24 12:01:48 +0800

    code建置viewcontroller

--- GitHubDemo/GitHubDemo/RootViewController.h
+++ GitHubDemo/GitHubDemo/RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  RootViewController.h
 //  GitHubDemo
 //
 //  Created by ryan.huang on 2020/3/24.
@@ -8,9 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+NS_ASSUME_NONNULL_BEGIN
 
-@property (strong, nonatomic) UIWindow * window;
+@interface RootViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
